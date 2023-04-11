a27b18360f7b17441245a9cb72fccf6cb249b4a1
SeanLo940076 <127285997+SeanLo940076@users.noreply.github.com>
2023-04-11 15:26:27 -0700

    update annotation

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from moviepy.editor import *
 # load the video
 clip = VideoFileClip("Demo.mp4")
 
-# getting only 3 first seconds from video
+# getting only 10 seconds from video
 clip = clip.subclip(1, 10)
 clip = clip.resize((640, 480))
 
 # save the video clip as gif
 #clip.write_gif("KITTI_0407.gif")
-clip.write_gif('output.gif', fps=30, opt='fast', fuzz=10)
+clip.write_gif('Demo.gif', fps=30, opt='fast', fuzz=10)
